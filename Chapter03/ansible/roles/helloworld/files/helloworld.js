933854afbbf751b54b3a808b2eae2bd4637bb7a1
kizax <kizax211075@gmail.com>
2020-02-05 17:02:30 +0800

    modify helloworld

--- Chapter03/ansible/roles/helloworld/files/helloworld.js
+++ Chapter03/ansible/roles/helloworld/files/helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
 
    // Send the response body as "Hello World"
-   response.end('Hello World, Welcome again 3\n');
+   response.end('Hello World, Welcome again 1\n');
 }).listen(3000);
 
 // Console will print the message
