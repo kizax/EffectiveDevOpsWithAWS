fbca4c4c9501458d956dc98650a1feb4f13ef4d5
kizax <kizax211075@gmail.com>
2020-02-12 11:56:55 +0800

    modify dir name

--- temp_Chapter05/EffectiveDevOpsTemplates/nodeserver-cf-template.py
+++ temp_Chapter05/EffectiveDevOpsTemplates/nodeserver-cf-template.py
@@ -203,28 +203,6 @@ t.add_resource(IAMPolicy(
     Roles=[Ref("Role")]
 ))
 
-t.add_resource(IAMPolicy(
-    "MonitoringPolicy",
-    PolicyName="AllowSendingDataForMonitoring",
-    PolicyDocument=Policy(
-        Statement=[
-            Statement(
-                Effect=Allow,
-                Action=[
-                    Action("cloudwatch", "Put*"),
-                    Action("logs", "Create*"),
-                    Action("logs", "Put*"),
-                    Action("logs", "Describe*"),
-                    Action("events", "Put*"),
-                    Action("firehose", "Put*"),
-                    Action("autoscaling", "DescribeAutoScalingInstances"),
-                ],
-                Resource=["*"])
-        ]
-    ),
-    Roles=[Ref("Role")]
-))
-
 t.add_resource(LaunchConfiguration(
     "LaunchConfiguration",
     UserData=ud,
