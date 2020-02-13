a67800b8e2314a7b298477d4721e4b12868a8686
kizax <kizax211075@gmail.com>
2020-02-13 22:01:29 +0800

    modify ami

--- Chapter05/EffectiveDevOpsTemplates/ecs-cluster-cf-template.py
+++ Chapter05/EffectiveDevOpsTemplates/ecs-cluster-cf-template.py
@@ -122,7 +122,7 @@ t.add_resource(LaunchConfiguration(
         "         --region ",
         Ref('AWS::Region'),
         "\n"])),
-    ImageId='ami-a0cfeed8',
+    ImageId='ami-00166aa2554456d3e',
     KeyName=Ref("KeyPair"),
     SecurityGroups=[Ref("SecurityGroup")],
     IamInstanceProfile=Ref('EC2InstanceProfile'),
