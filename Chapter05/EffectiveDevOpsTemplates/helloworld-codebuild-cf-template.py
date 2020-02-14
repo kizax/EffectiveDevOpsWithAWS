09b8393201014da7e15068f7b89bbdf516dc063b
kizax <kizax211075@gmail.com>
2020-02-14 22:55:44 +0800

    modify template

--- Chapter05/EffectiveDevOpsTemplates/helloworld-codebuild-cf-template.py
+++ Chapter05/EffectiveDevOpsTemplates/helloworld-codebuild-cf-template.py
@@ -85,7 +85,7 @@ phases:
       - aws ecr batch-get-image --repository-name $REPOSITORY_NAME --image-ids imageTag="$(cat /tmp/tag.txt)" --query 'images[].imageManifest' --output text | tee /tmp/latest_manifest.json
       - cat /tmp/latest_manifest.json
       - echo $REPOSITORY_NAME
-      - aws ecr put-image --repository-name $REPOSITORY_NAME --image-tag latest --image-manifest $(cat /tmp/latest_manifest.json)
+      - aws ecr put-image --repository-name $REPOSITORY_NAME --image-tag latest --image-manifest file://tmp/latest_manifest.json
 artifacts:
   files: /tmp/build.json
   discard-paths: yes
